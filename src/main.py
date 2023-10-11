743ee49d5b0f0dc619d6d8269fae0ef2c20bf4fc
Ya095 <egor.yakovlev.95@gmail.com>
2023-10-11 18:43:05 +0300

    test commit3.2

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 
+
 app = FastAPI(
     title='Deposit calculation',
     description='Сервис для расчета депозита.'
